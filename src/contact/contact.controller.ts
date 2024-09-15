2ac1f9796f178be5d8fa44e72b5ffcc9c9d2d24a
knyongki <yyongkisetiawan@gmail.com>
2024-09-16 00:16:19 +0700

    search contact implemeted

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -8,11 +8,13 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import {
   ContactResponse,
   CreateContactRequest,
+  SearchContactRequest,
   UpdateContactRequest,
 } from '../model/contact.model';
 import { User } from '@prisma/client';
@@ -71,5 +73,25 @@ export class ContactController {
     return {
       data: true,
     };
+  }
+
+  @Get()
+  @HttpCode(200)
+  async search(
+    @Auth() user: User,
+    @Query('name') name?: string,
+    @Query('email') email?: string,
+    @Query('phone') phone?: string,
+    @Query('page', new ParseIntPipe({ optional: true })) page?: number,
+    @Query('size', new ParseIntPipe({ optional: true })) size?: number,
+  ): Promise<WebResponse<ContactResponse[]>> {
+    const request: SearchContactRequest = {
+      name: name,
+      email: email,
+      phone: phone,
+      page: page || 1,
+      size: size || 10,
+    };
+    return this.contactService.search(user, request);
   }
 }
